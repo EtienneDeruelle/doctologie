4f15f6a274840df0ad682baed6cfff7a0cc02e32
Patapignouf <quentinkhoanguyen@gmail.com>
2017-04-18 21:49:24 +0200

    Interface encore non fonctionnelle
    Début interface graphique
    Petites corrections dans la recherche

---
+++ src/display/ButtonWindow.java
@@ -0,0 +1,48 @@
+package display;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.io.IOException;
+
+
+import javax.swing.JPanel;
+
+
+public class ButtonWindow extends JPanel {
+	/*
+	private ButtonConnexion boutonA = new ButtonConnexion("");
+	private ButtonOffline boutonO = new ButtonOffline("");
+	private ButtonDL boutonD = new ButtonDL();
+	
+	
+	public ButtonWindow () throws IOException {
+		this.setLayout(new BorderLayout());
+		this.setOpaque(false);
+		this.setVisible(true);
+		
+		JPanel SectionB = new JPanel();
+		this.add(SectionB, BorderLayout.EAST); 
+		
+		SectionB.setLayout(new GridLayout(6,1));
+		SectionB.add(boutonA);
+		SectionB.add(boutonO);
+		SectionB.add(boutonD);
+		
+		SectionB.setOpaque(false);
+		SectionB.setVisible(true);
+	}
+	
+	public ButtonConnexion getBoutonA() {
+		return boutonA;
+	}
+
+	public ButtonOffline getBoutonO() {
+		return boutonO;
+	}
+
+	public ButtonDL getBoutonD() {
+		return boutonD;
+	}
+	
+	*/
+}
