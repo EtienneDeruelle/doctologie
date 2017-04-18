131ab526abbfe761e08991d50aea2117a0a854af
Patapignouf <quentinkhoanguyen@gmail.com>
2017-04-18 21:59:32 +0200

    Faut se bouger pour faire un main qu'on peut tous exploiter xD

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,15 +10,17 @@ import org.lightcouch.CouchDbClient;
 import connection.ConnectionCouchDB;
 import connection.ConnectionSQLite;
 import sqlite.SQLiteRequest;
-
+import display.MainWindow;
 
 
 public class Main {
-
+	
+	private static MainWindow window ;
+	
 	public static void main(String[] args) {
 		SQLiteRequest.requestByIdDisease(14);
 		
-		
+		window = new MainWindow();
 		
 	}
 
