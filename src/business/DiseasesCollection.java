db19468a031ff03a0444fe7f5b44547f5d3b04e5
Emanuel MARTINS <manu_57190@hotmail.fr>
2017-05-15 09:33:47 +0200

    commentaires anglais sur DiseasesCollection

--- src/business/DiseasesCollection.java
+++ src/business/DiseasesCollection.java
@@ -7,21 +7,45 @@ public class DiseasesCollection {
 	private ArrayList<String> genteticalDiseases;
 	private ArrayList<String> rareDiseases;
 	
+	/**
+	 * Constructor of the class
+	 * @param genteticalDiseases
+	 * @param rareDiseases
+	 */
 	public DiseasesCollection(ArrayList<String> genteticalDiseases, ArrayList<String> rareDiseases) {
 		super();
 		this.genteticalDiseases = genteticalDiseases;
 		this.rareDiseases = rareDiseases;
 	}
 	
+	/**
+	 * Recovers genetic diseases
+	 * @return a ArralyList of String(genetic diseases)
+	 */
 	public ArrayList<String> getGenteticalDiseases() {
 		return genteticalDiseases;
 	}
+	
+	/**
+	 * Allows to modify genetic diseases
+	 * @param genteticalDiseases
+	 */
 	public void setGenteticalDiseases(ArrayList<String> genteticalDiseases) {
 		this.genteticalDiseases = genteticalDiseases;
 	}
+	
+	/**
+	 * Recovers rare diseases
+	 * @return
+	 */
 	public ArrayList<String> getRareDiseases() {
 		return rareDiseases;
 	}
+	
+	/**
+	 * Recovers rare diseases
+	 * @param rareDiseases
+	 */
 	public void setRareDiseases(ArrayList<String> rareDiseases) {
 		this.rareDiseases = rareDiseases;
 	}
